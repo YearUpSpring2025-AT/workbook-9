a5c31a239b65a56cb1fc5f867f4f569f9a66ba14
abdule18 <101951085+abdule18@users.noreply.github.com>
2025-06-16 08:41:45 -0400

    Class demo

---
+++ classDemo1/src/main/java/com/pluralsight/classDemo1/ClassDemo1Application.java
@@ -0,0 +1,13 @@
+package com.pluralsight.classDemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassDemo1Application.class, args);
+	}
+
+}
---
+++ classDemo1/src/test/java/com/pluralsight/classDemo1/ClassDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.classDemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
